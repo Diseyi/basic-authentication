816db9b7a606bfaa0eca071c0b565c8c952698f9
diseyi <filomiinaa@gmail.com>
2022-11-02 13:10:16 +0100

    chore: add dotenv package

--- index.ts
+++ index.ts
@@ -1,9 +1,12 @@
 
-import express, { Application, Request, Response } from "express";
+import express, { Application} from "express";
+import * as dotenv from "dotenv";
 import userRouter from "./routes/userRoutes";
 
+dotenv.config();
+
 const app: Application = express()
-const PORT = 4040
+const PORT = process.env.PORT
 
 app.use(express.json())
 
